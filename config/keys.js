29e3d8f055facc20dfed3b80d5a0115ffc5de13b
Soumya Ranjan Mohanty <soumyacool2012@gmail.com>
2018-11-13 23:19:12 +0530

    Connect to mongoDB

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: "mongodb://srm:srm12345@ds161653.mlab.com:61653/mern-blog"
+};
--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
+
+// MongoDB configuration
+const db = require("./config/keys").mongoURI;
+
+// Use mongoose to connect to mongoDB
+mongoose
+  .connect(db)
+  .then(() => console.log("MongoDB connected successfully!"))
+  .catch(err => console.log(err));
 
 app.get("/", (req, res) =>
   res.send("Welcome to MERN stack blog with google oauth")
