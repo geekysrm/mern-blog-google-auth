9db702112d217cbeeb0fdcb889f0f670483bf38f
Soumya Ranjan Mohanty <soumyacool2012@gmail.com>
2018-11-15 20:14:38 +0530

    Setup redux

---
+++ client/src/reducers/index.js
@@ -0,0 +1,8 @@
+// Root Reducer
+import { combineReducers } from "redux";
+// import authReducer from "./authReducer";
+// import errorReducer from "./errorReducer";
+// import profileReducer from "./profileReducer";
+// import postReducer from "./postReducer";
+
+export default combineReducers({});
