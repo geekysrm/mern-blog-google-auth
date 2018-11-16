05b244a4a26b46298c35e7d926edbbad39eca6ba
Soumya Ranjan Mohanty <soumyacool2012@gmail.com>
2018-11-17 02:29:59 +0530

    Minor fixes

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -26,7 +26,7 @@ module.exports = app => {
 
   app.get("/api/logout", (req, res) => {
     req.logout();
-    res.redirect("/login");
+    res.redirect("/");
   });
 
   app.get("/api/current_user", (req, res) => {
