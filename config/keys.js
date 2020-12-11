b2c66c0fe1e16206811104fd32c9efc190187a0e
geekysrm <soumyacool2012@gmail.com>
2020-12-12 03:15:21 +0530

    update keys

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,8 @@
 module.exports = {
-//   mongoURI: "mongodb://srm:srm12345@ds161653.mlab.com:61653/mern-blog",
-  mongoURI: "mongodb+srv://srm:<password>@mern-blog.xzrsp.mongodb.net/mern-blog?retryWrites=true&w=majority",
-  googleClientID: '348653924741-br3pl65kjru675evlohd9fjdj7o86sla.apps.googleusercontent.com',
-  googleClientSecret: '_98wmUEJOBAdxMg8YU6yeXnR',
+  //   mongoURI: "mongodb://srm:srm12345@ds161653.mlab.com:61653/mern-blog",
+  mongoURI:
+    "mongodb+srv://srm:srm12345@mern-blog.xzrsp.mongodb.net/mern-blog?retryWrites=true&w=majority",
+  googleClientID:
+    "348653924741-br3pl65kjru675evlohd9fjdj7o86sla.apps.googleusercontent.com",
+  googleClientSecret: "_98wmUEJOBAdxMg8YU6yeXnR",
 };
